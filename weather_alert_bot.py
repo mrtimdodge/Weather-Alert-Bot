46322d9946c0d49c0f40eac2265fdc063e496e9f
mrtimdodge <mrtimdodge@gmail.com>
2016-03-31 13:07:08 -0500

    update so the bot edits post instead of creating new one.

--- weather_alert_bot.py
+++ weather_alert_bot.py
@@ -10,6 +10,7 @@ import obot_weather as obot
 alert_SAMECode = ""
 alert_state = "AR"
 alert_county = ""
+post_id = ""
 subreddit = "DapperDodger"
 app_ua = 'Weather-Alert-Bot is a Reddit bot intended to be used for location specific subreddits to provide weather alerts.'
 
@@ -19,8 +20,15 @@ r = obot.login()
 if __name__=="__main__":
     nws = weatheralerts.WeatherAlerts(state=alert_state)
     for alert in nws.alerts:
-        if alert.severity == 'Severe':
-            text =  alert.title + " " + alert.areadesc
-            r.submit(subreddit,"Severe Weather Alert",text)
+        if alert.severity == 'Severe'and "Severe Weather Statement" not in alert.title:
+            text =  alert.title + "\n\n Counties Affected: " + alert.areadesc
+            if post_id and post_id != "":
+                submiss = r.get_submission(submission_id=post_id)
+                submiss.refresh()
+                print submiss.selftext
+                submiss.edit(submiss.selftext + "\n\n" + text + "\n\n" + "------------------------------------------------------")
+            else:
+                post_id = r.submit(subreddit,"Severe Weather Alert",text + "\n\n" + "------------------------------------------------------").id
+
         
     print "done"
